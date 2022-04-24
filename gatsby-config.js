43c626135c8fc80decc264c39f44a1af1dbed629
Sangwook Lee <leesang627@gmail.com>
2022-04-24 21:31:26 +0900

    add google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,17 @@ module.exports = {
     siteUrl: `https://www.yourdomain.tld`,
   },
   plugins: [
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          'G-T46Q28G7H7', // 설정 Google Analytics / GA
+          // "AW-CONVERSION_ID", // Google Ads / Adwords / AW
+          // "DC-FLOODIGHT_ID", // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
+        ],
+      },
+    },
     'gatsby-plugin-postcss',
     'gatsby-plugin-image',
     'gatsby-plugin-react-helmet',
@@ -93,6 +104,7 @@ module.exports = {
         name: `info`,
         path: `${__dirname}/contents/info`,
       },
-    },
+    }
+    
   ],
 };
